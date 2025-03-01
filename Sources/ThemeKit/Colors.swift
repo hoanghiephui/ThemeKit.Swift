7787923e8080bf907d8522af4ab848d243dfe0e6
Hoang Hiep <hoanghiep.hui@gmail.com>
2025-03-01 20:18:15 +0700

    update color
    Signed-off-by: Hoang Hiep <hoanghiep.hui@gmail.com>

--- Sources/ThemeKit/Colors.swift
+++ Sources/ThemeKit/Colors.swift
@@ -47,7 +47,7 @@ extension Color {
 
 extension UIColor {
     public static let themeYellowD = UIColor(hex: 0xffa800)
-    public static let themeYellowL = UIColor(hex: 0xff8a00)
+    public static let themeYellowL = UIColor(hex: 0x005EFE)
     public static let themeGreenD = UIColor(hex: 0x05c46b)
     public static let themeGreenL = UIColor(hex: 0x04ad5f)
     public static let themeRedD = UIColor(hex: 0xf43a4f)
